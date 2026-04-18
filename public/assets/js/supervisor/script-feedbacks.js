bf167b1c71136cbc7d751c99ddb5bd59c6313387
Sesandi Nethmini <127113583+Sesandii@users.noreply.github.com>
2026-04-18 13:25:50 +0530

    issues

--- public/assets/js/supervisor/script-feedbacks.js
+++
@@ -1,8 +0,0 @@
-// Simple search filter
-document.querySelector(".filter-search").addEventListener("input", function () {
-  let query = this.value.toLowerCase();
-  document.querySelectorAll(".card").forEach(card => {
-    let text = card.innerText.toLowerCase();
-    card.style.display = text.includes(query) ? "block" : "none";
-  });
-});
--- public/assets/js/supervisor/script-report.js
+++
@@ -1,8 +0,0 @@
-function saveDraft() {
-  alert("Draft saved successfully!");
-}
-
-function submitReport() {
-  alert("Final report submitted successfully!");
-}
-
