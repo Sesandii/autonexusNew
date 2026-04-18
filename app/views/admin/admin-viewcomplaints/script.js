f1063e03d016ba3a493213b3593d8cb470ebd127
Sesandi Nethmini <127113583+Sesandii@users.noreply.github.com>
2026-04-18 13:41:46 +0530

    issues

--- app/views/admin/admin-viewcomplaints/script.js
+++
@@ -1,9 +0,0 @@
-// Highlight active sidebar menu item
-const menuItems = document.querySelectorAll(".menu li");
-
-menuItems.forEach(item => {
-    item.addEventListener("click", () => {
-        document.querySelector(".menu li.active").classList.remove("active");
-        item.classList.add("active");
-    });
-});
